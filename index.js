65a473f5f9d81e5ab957c7e04008a7b063dc2aaf
Altamash Ahmad <almasahmad8126@gmail.com>
2024-01-28 18:45:42 +0530

    Quora files

---
+++ index.js
@@ -0,0 +1,78 @@
+const express =require("express");
+const app =express();
+const port =8080;
+const path= require("path");
+const { v4: uuidv4 } = require('uuid');   
+var methodOverride = require('method-override');
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.set("view engine","ejs");
+app.set("views" , path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
+
+
+let posts=[
+    {
+        id:uuidv4(),
+        username:"Altamash",
+        comment: "love to do coding",
+    },
+    {
+        id:uuidv4(),
+        username:"Ahmad",
+        comment: "love to do coding",
+    },
+    {
+        id:uuidv4(),
+        username:"zaid",
+        comment: "love to do coding",
+    }
+]
+app.get("/posts",(req, res)=>{
+   
+    res.render("index.ejs", {posts} )
+  
+});
+
+app.get("/posts/new",(req,res)=>{
+res.render("new.ejs")
+});
+ 
+app.post("/posts",(req,res)=>{
+    let { username, comment }=req.body;
+    let id=uuidv4();
+    posts.push({id, username ,comment });
+    res.redirect("/posts");
+});
+app.get("/posts/:id",(req,res)=>{
+    let {id} =req.params;
+    let post= posts.find((p)=>id===p.id);
+    res.render("detail.ejs",{post});
+});
+app.patch("/posts/:id",(req,res)=>{
+    let {id }=req.params;
+    id =id.trim();
+    let newComment=req.body.comment;
+    let post= posts.find((p)=>id===p.id);
+    post.comment=newComment;
+    res.redirect("/posts");
+
+});
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id} =req.params;
+    id =id.trim(); 
+    let post= posts.find((p)=>id===p.id);
+    res.render("edit.ejs",{post} );
+})
+
+app.delete("/posts/:id",(req,res)=>{
+    let {id} =req.params;
+    posts= posts.filter((p)=>id!==p.id);
+    res.redirect("/posts")
+})
+
+
+
+app.listen(port,()=>{
+    console.log("port is listening")
+});
